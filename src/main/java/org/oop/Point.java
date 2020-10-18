79a08c8aba4fac350e08f935605c88d944fdf605
Anusha Kalyani <anushakalyani@Anushas-MacBook-Pro.local>
2020-10-18 17:28:59 +0530

    Converted the code into object oriented

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -3,25 +3,17 @@ package org.oop;
 public class Point {
     private double x;
     private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+    
+    public static double calculateDistance(Point from, Point to){
+        return Math.sqrt(Math.pow((to.x-from.x),2)+ Math.pow((to.y-from.y),2));
+    }
+    public static double calculateDirection(Point from, Point to) {
+        return Math.atan2((to.y-from.y),(to.x-from.x) );
+    }
+
+
 }
